de8b8c06e00a349bfd8ac3e4418f051d9d58ceda
nour.saidi <nour.saidi@backmarket.com>
2024-10-11 20:15:26 +0200

    Add domain brand client

---
+++ app/src/main/java/com/example/radiofrancestation/domain/model/BrandDetailDomain.kt
@@ -0,0 +1,9 @@
+package com.example.radiofrancestation.domain.model
+
+data class BrandDetailDomain(
+    val id: String,
+    val title: String,
+    val description: String?,
+    val baseLine: String?
+) {
+}
---
+++ app/src/main/java/com/example/radiofrancestation/domain/model/BrandDomain.kt
@@ -0,0 +1,8 @@
+package com.example.radiofrancestation.domain.model
+
+data class BrandDomain(
+    val id: String,
+    val title: String,
+    val baseLine: String?
+) {
+}
